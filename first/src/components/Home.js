ad73ec6dca2ce72451b4f3aa63cf362104c8578c
husenjanobul <husenjanobul@gmail.com>
2020-02-11 18:46:31 +0100

    test3

---
+++ first/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+    const husenjan = 'obul'
+    return (
+        <div>
+            Home
+            
+        </div>
+    )
+}
+
+export default Home
